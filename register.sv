dae27aa01601bdcb48f45c427cf6ac7fd4f3d7f8
YZXia-IC <116905738+YZXia-IC@users.noreply.github.com>
2022-11-17 19:54:03 +0000

    Update register.sv

--- register.sv
+++ register.sv
@@ -30,22 +30,6 @@ endmodule
 
 
 
-module mux2(
-    input logic [31:0] regOp2,Immop
-    input logic  ALUsrc,
-    output logic [31:0] Aluop1);
-
-
-    assign Aluop1 = s ? regOp2 : Immop;
-endmodule
-
-
-
-
-
-)
-
-
 
 
 
