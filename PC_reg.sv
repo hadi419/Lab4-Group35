323b3a7891ead0a77b832f7b4144d758a3834170
Wangy1Lu <115638700+Wangy1Lu@users.noreply.github.com>
2022-11-21 01:31:56 +0000

    Create PC_reg.sv

---
+++ PC_reg.sv
@@ -0,0 +1,14 @@
+module PC_reg #(
+ parameter WIDTH = 32
+)(
+ input  logic                clk, 
+ input  logic                rst,  
+ input  logic [WIDTH-1:0]    next_PC,
+ output logic [WIDTH-1:0]    PC
+); 
+
+always_ff @ (posedge clk, posedge rst)
+ if (rst)count <= 32'b0;
+ else count <= next_PC;
+
+endmodule
