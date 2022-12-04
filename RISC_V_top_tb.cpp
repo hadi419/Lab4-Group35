841a1a2282aa28088dfdbd7a3d6a443597dbe84c
devbanthia <116261494+devbanthia@users.noreply.github.com>
2022-12-04 22:08:07 +0530

    Add files via upload

--- RISC_V_top_tb.cpp
+++ RISC_V_top_tb.cpp
@@ -1,46 +1,49 @@
-#include "VRISC_V_top.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include "VRISC_V_top.h"
 
-int main(int argc, char**argv, char **env)
-{
-    int i;
-    int clk;
+#include "vbuddy.cpp"     // include vbuddy code
+#define MAX_SIM_CYC 100000
 
-    Verilated::commandArgs(argc,argv);
-
-    VRISC_V_top* top=new VRISC_V_top;
-    Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp=new VerilatedVcdC;
-    top->trace(tfp,99);
-    tfp->open("RISC_V_top.vcd");
-
-    top->clk=1;
-    top->rst=0;
-
-
+int main(int argc, char **argv, char **env) {
+  int simcyc;     // simulation clock count
+  int tick;       // each clk cycle has two ticks for two edges
   
+  Verilated::commandArgs(argc, argv);
+  // init top verilog instance
+  VRISC_V_top * top = new VRISC_V_top;
+  // init trace dump
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("RISC_V_top.vcd");
+ 
+  // init Vbuddy
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("L3T3:timed_f1_fsm");
+  vbdSetMode(1);        // Flag mode set to one-shot
 
-    for(i=0;i<300;i++)
-    {
-        for(clk=0;clk<2;clk++)
-        {
-            tfp->dump(2*i+clk);
-            top->clk=!top->clk;
-            top->eval();
-        }
-
-        
-        vbdBar(top->a0);
-        
-        
-
-
-        if(Verilated::gotFinish()) exit(0);
+  // initialize simulation inputs
+  top->clk = 1;
+  top->rst = 0;
+  
+  // run simulation for MAX_SIM_CYC clock cycles
+  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
     }
 
-    tfp->close();
-    exit(0);
+    // Display toggle neopixel
+    vbdBar(top->a0);
+    // set up input signals of testbench
+ 
+    if (Verilated::gotFinish())  exit(0);
+  }
 
-
+  vbdClose();     // ++++
+  tfp->close(); 
+  exit(0);
 }
